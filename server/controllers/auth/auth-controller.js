f4baab768a02b6837d2db3ed36a88587d5f54558
quoc127 <nquoc160200@gmail.com>
2024-09-30 11:01:24 +0700

    create register and login API

---
+++ server/controllers/auth/auth-controller.js
@@ -0,0 +1,89 @@
+const User = require("../../models/Users");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+module.exports.register = async (req, res) => {
+  const { userName, email, password } = req.body;
+
+  try {
+    const checkUser = await User.findOne({
+      email: email,
+    });
+
+    if (checkUser) {
+      return res.json({
+        success: false,
+        message: "User Already exists with the same email! Please try again",
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 12);
+
+    const newUser = new User({
+      userName: userName,
+      email: email,
+      password: hashPassword,
+    });
+
+    newUser.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Create new user successfuly",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+module.exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const checkUser = await User.findOne({ email: email });
+  try {
+    if (!checkUser) {
+      return res.json({
+        success: false,
+        message: "User doesn't exists! Please register first",
+      });
+    }
+
+    const checkPasswordMatch = await bcrypt.compare(
+      password,
+      checkUser.password
+    );
+    if (!checkPasswordMatch) {
+      return res.json({
+        success: false,
+        message: "Incorrect password! Please try again.",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userName: checkUser.userName,
+        email: checkUser.email,
+        role: checkUser.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "60m",
+      }
+    );
+    res.status(200).json({
+      success: true,
+      message: "Login Successfuly!",
+      token: token
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+const express = require("express")
+
+const app = express();
+
+const bodyParser = require("body-parser");
+// Support parsing of application/json type post data
+app.use(bodyParser.json());
+
+// Support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Databasse
+const dbConnect = require("./config/DBconnect")
+dbConnect.dbConnect();
+
+// Routes
+const RoutesAPIVer1 = require("./routes/index-routes")
+RoutesAPIVer1(app)
+
+// Start server
+const PORT = process.env.PORT
+app.listen((PORT), () => {
+  console.log(`Server is running at port ${PORT}`);
+})
