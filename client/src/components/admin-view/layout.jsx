d5ae40ff9e01e97223da83991dea619692aa7111
quoc127 <nquoc160200@gmail.com>
2024-10-08 22:46:15 +0700

    design dashboard

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,13 +1,24 @@
 import { Outlet } from "react-router-dom";
+import AdminSideBar from "./side-bard";
+import { useState } from "react";
+import { AdminHeader } from "./header";
 
 export const AdminLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <div className="flex min-h-screen w-full">
-      <div>admin sidebar</div>
-      <div className="flex flex-1 flex-col">admin header</div>
-      <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
-        <Outlet />
-      </main>
+      <div>
+        <AdminSideBar
+          openSidebar={openSidebar}
+          setOpenSidebar={setOpenSidebar}
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <AdminHeader setOpenSidebar={setOpenSidebar} />
+        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
