fd413da773f5be371da45ecdb4b60cf8cbfb0497
quoc127 <nquoc160200@gmail.com>
2024-10-18 16:01:21 +0700

    update product-validate.js

--- server/validates/admin/product/product-validates.js
+++ server/validates/admin/product/product-validates.js
@@ -27,9 +27,10 @@ module.exports.AddProduct = (req, res, next) => {
 
 module.exports.EditProduct = (req, res, next) => {
   const { name, price, brandId, categoryId } = req.body;
+  const priceType = parseInt(price)
 
   if (validateField("Product name", name, "string", res)) return;
-  if (validateField("Price", price, "number", res)) return;
+  if (validateField("Price", priceType, "number", res)) return;
   if (validateField("Brand ID", brandId, "string", res)) return;
   if (validateField("Category ID", categoryId, "string", res)) return;
 
