bcead91b8a0f1ceb4b0f9efdac8f30277642a7a5
quoc127 <nquoc160200@gmail.com>
2024-11-06 17:15:11 +0700

    design home page shop

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -1,5 +1,99 @@
+import { useEffect, useState } from "react";
+import BannerOne from "../../assets/banner-1.webp";
+import BannerTwo from "../../assets/banner-2.webp";
+import BannerThree from "../../assets/banner-3.webp";
+import { Button } from "@/components/ui/button";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { ShoppingProductTile } from "@/components/shopping-view/product-tile";
+import { useDispatch, useSelector } from "react-redux";
+import { getShopAllProducts } from "@/store/shop-slice/products-slice";
+
+const banners = [BannerOne, BannerTwo, BannerThree];
+
 export const ShoppingHome = () => {
-  return(
-    <div>this is home page</div>
-  )
-}
+  const { productList } = useSelector((state) => state.shopProducts);
+  const dispatch = useDispatch();
+  const [currentSlide, setCurrentSlide] = useState(0);
+  console.log("productList", productList);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % banners.length);
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, [banners]);
+
+  useEffect(() => {
+    dispatch(getShopAllProducts());
+  }, [dispatch]);
+
+  return (
+    <div className="flex flex-col p-6">
+      <div className="relative w-full h-[600px] overflow-hidden">
+        {banners && banners.length > 0
+          ? banners.map((slide, index) => (
+              <img
+                src={slide}
+                key={index}
+                className={`${
+                  index === currentSlide ? "opacity-100" : "opacity-0"
+                } absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000`}
+              />
+            ))
+          : null}
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() =>
+            setCurrentSlide(
+              (prevSlide) => (prevSlide - 1 + banners.length) % banners.length
+            )
+          }
+          className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white/80"
+        >
+          <ChevronLeftIcon className="w-4 h-4" />
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() =>
+            setCurrentSlide((prevSlide) => (prevSlide + 1) % banners.length)
+          }
+          className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white/80"
+        >
+          <ChevronRightIcon className="w-4 h-4" />
+        </Button>
+      </div>
+
+      <section className="gap-6 p-4 md:p-6">
+        <h2 className="text-3xl font-bold text-center mb-8">New Products</h2>
+        <div className="mx-auto max-w-2xl px-4 sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {productList && productList.length > 0
+              ? productList.map((productItem, index) => {
+                  return (
+                    <div key={index} className="border">
+                      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                        <img
+                          alt={productItem.name}
+                          src={productItem.image}
+                          className="h-full w-full object-cover object-center group-hover:opacity-75"
+                        />
+                      </div>
+                      <h3 className="mt-4 text-sm text-gray-700">
+                        {productItem.name}
+                      </h3>
+                      <p className="mt-1 text-lg font-medium text-gray-900">
+                        {productItem.price}
+                      </p>
+                    </div>
+                  );
+                })
+              : null}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
