810dc148764b021a89e1d9c94c027ac8384eee41
quoc127 <nquoc160200@gmail.com>
2024-11-19 21:23:37 +0700

    responsive shop header

--- client/src/components/shopping-view/shopping-header.jsx
+++ client/src/components/shopping-view/shopping-header.jsx
@@ -23,6 +23,7 @@ import { logoutUser } from "@/store/auth-slice";
 import { Input } from "../ui/input";
 import { useEffect, useState } from "react";
 import { getShopAllProductsSearch } from "@/store/shop-slice/products-slice";
+import { SearchInput } from "./shopping-header/search-input";
 
 const MenuItems = () => {
   const navigate = useNavigate();
@@ -114,73 +115,54 @@ export const ShoppingHeader = ({
 }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  
+
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="flex px-4 items-center justify-between h-16">
-        <Link to="/shop/home" className="flex items-center gap-2">
-          <img src="https://websitedemos.net/t-shirts-store-04/wp-content/uploads/sites/1115/2022/07/logo-regular.png"></img>
-        </Link>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="lg:hidden">
-              <Menu className="h-6 w-6" />
-              <span className="sr-only">Toggle header menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left" className="w-full max-w-xs">
+      <div className="px-4 flex flex-col lg:flex-col xl:flex-row items-center justify-between gap-4">
+        {/* Hàng đầu tiên */}
+        <div className="flex items-center justify-between lg:justify-end w-full">
+          <Link to="/shop/home" className="flex items-center gap-2">
+            <img
+              src="https://websitedemos.net/t-shirts-store-04/wp-content/uploads/sites/1115/2022/07/logo-regular.png"
+              alt="Logo"
+            />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="lg:hidden">
+                <Menu className="h-6 w-6" />
+                <span className="sr-only">Toggle header menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="w-full max-w-xs">
+              <MenuItems />
+              <HeaderRightContent />
+            </SheetContent>
+          </Sheet>
+          <div className="hidden lg:block mx-auto">
             <MenuItems />
+          </div>
+          <div className="hidden lg:block xl:hidden">
             <HeaderRightContent />
-          </SheetContent>
-        </Sheet>
-        <div className="hidden lg:block">
-          <MenuItems />
-        </div>
-        <div className="relative flex-1 md:grow-0">
-          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input
-            value={keyword}
-            onKeyDown={(event) => {
-              if (event.key === "Enter" && keyword && searchResults) {
-                navigate("/shop/products-list");
-                setCompleteSearch(true);
-              }
-            }}
-            onChange={(event) => {
-              handleInput(event);
-              setCompleteSearch(false);
-            }}
-            type="search"
-            placeholder="Search..."
-            className="w-full pl-8 md:w-[200px] lg:w-[336px]"
-          />
-          {keyword && searchResults.length > 0 && (
-            <div
-              className={`${
-                completeSearch ? "hidden" : "block"
-              } absolute z-10 mt-2 w-full max-h-[300px] overflow-y-auto bg-white border border-gray-300 rounded-lg shadow-lg`}
-            >
-              {searchResults.map((item, index) => (
-                <div key={index} className="flex flex-row m-2 pb-2 border-b-2">
-                  <img src={item.image} width="50px" className="object-cover" />
-                  <div
-                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-sm w-full"
-                    onClick={() => {
-                      navigate(`/shop/product-detail/${item._id}`);
-                      setKeyword("");
-                      setSearchResults([]);
-                    }}
-                  >
-                    {item.name}
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
+          </div>
         </div>
 
-        <div className="hidden lg:block">
-          <HeaderRightContent />
+        {/* Hàng thứ hai */}
+        <div className="flex items-center justify-between w-full xl:max-w-[400px] mb-2 xl:my-0 gap-4">
+          <div className="w-full lg:max-w-[400px]">
+            <SearchInput
+              keyword={keyword}
+              setKeyword={setKeyword}
+              searchResults={searchResults}
+              setSearchResults={setSearchResults}
+              completeSearch={completeSearch}
+              setCompleteSearch={setCompleteSearch}
+              handleInput={handleInput}
+            />
+          </div>
+          <div className="hidden xl:block">
+            <HeaderRightContent />
+          </div>
         </div>
       </div>
     </header>
