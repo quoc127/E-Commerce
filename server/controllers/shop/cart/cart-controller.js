c8e192c0df3bb8d358d2dfc7217f1ae22d0ac547
quoc127 <nquoc160200@gmail.com>
2024-11-26 16:04:43 +0700

    create API update and delete cart item for shop

--- server/controllers/shop/cart/cart-controller.js
+++ server/controllers/shop/cart/cart-controller.js
@@ -64,12 +64,11 @@ module.exports.getCartItems = async (req, res) => {
       select: "name image price",
     });
     console.log(cart);
-    
 
     const validItems = cart.items.filter(
       (productItem) => productItem.productId
     );
-    
+
     if (validItems.length < cart.items.length) {
       cart.items = validItems;
       await cart.save();
@@ -84,6 +83,122 @@ module.exports.getCartItems = async (req, res) => {
     }));
     res.status(200).json({
       success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+module.exports.updateCartItems = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || quantity <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
+    }
+
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found!",
+      });
+    }
+
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (findCurrentProductIndex === -1) {
+      return res.status(400).json({
+        success: false,
+        message: "Cart item not present !",
+      });
+    }
+
+    cart.items[findCurrentProductIndex].quantity = quantity;
+    await cart.save();
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      name: item.productId ? item.productId.name : "Product not found",
+      image: item.productId ? item.productId.image : null,
+      price: item.productId ? item.productId.price : null,
+      quantity: item.productId ? item.quantity : null,
+    }));
+
+    res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+module.exports.deleteCartItems = async (req, res) => {
+  try {
+    const { userId, productId } = req.params;
+
+    if (!userId || !productId) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
+    }
+
+    const cart = await Cart.findOne({ userId: userId }).populate({
+      path: "items.productId",
+      select: "name image price",
+    });
+    
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found!",
+      });
+    }
+    
+    cart.items = cart.items.filter((item) => item.productId._id.toString() !== productId);
+    await cart.save();
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      name: item.productId ? item.productId.name : "Product not found",
+      image: item.productId ? item.productId.image : null,
+      price: item.productId ? item.productId.price : null,
+      quantity: item.productId ? item.quantity : null,
+    }));
+
+    res.status(200).json({
+      success: true,
+      message: "Delete item successfully!",
       data: {
         ...cart._doc,
         items: populateCartItems,
--- server/routes/shop/cart/cart-routes.js
+++ server/routes/shop/cart/cart-routes.js
@@ -4,5 +4,7 @@ const controller = require("../../../controllers/shop/cart/cart-controller")
 
 route.post("/add", controller.addToCart)
 route.get("/get/:userId", controller.getCartItems)
+route.patch("/update", controller.updateCartItems)
+route.delete("/delete/:userId/:productId", controller.deleteCartItems)
 
 module.exports = route;
