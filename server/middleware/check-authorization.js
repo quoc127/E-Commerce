9f1c3a850d93fd8cb3695d29d24ef5a141cc1f74
quoc127 <nquoc160200@gmail.com>
2024-12-02 15:46:12 +0700

    update verifyToken with header

--- server/middleware/check-authorization.js
+++ server/middleware/check-authorization.js
@@ -1,48 +1,47 @@
 const jwt = require("jsonwebtoken");
 
-// module.exports.verifyToken = (req, res, next) => {
-//   const authHeader = req.header("Authorization");
-
-//   if (authHeader && authHeader.startsWith("Bearer")) {
-//     const token = authHeader.split(" ")[1];
-//     try {
-//       const decode = jwt.verify(token, process.env.JWT_ACESS_TOKEN_KEY);
-//       req.user = decode;
-//       next();
-//     } catch (error) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "Invalid token",
-//       });
-//     }
-//   } else {
-//     return res.status(401).json({
-//       success: false,
-//       message: "No token, authorization denied",
-//     });
-//   }
-// };
-
 module.exports.verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
+  const authHeader = req.header("Authorization");
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    const token = authHeader.split(" ")[1];
+    try {
+      const decode = jwt.verify(token, process.env.JWT_ACESS_TOKEN_KEY);
+      req.user = decode;
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
+  } else {
     return res.status(401).json({
       success: false,
       message: "No token, authorization denied",
     });
   }
-
-  try {
-    const decode = jwt.verify(token, process.env.JWT_ACESS_TOKEN_KEY);
-    req.user = decode;
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      success: false,
-      message: "Invalid token",
-    });
-  }
 };
+
+// module.exports.verifyToken = (req, res, next) => {
+//   const token = req.cookies.token;
+//   if (!token) {
+//     return res.status(401).json({
+//       success: false,
+//       message: "No token, authorization denied",
+//     });
+//   }
+
+//   try {
+//     const decode = jwt.verify(token, process.env.JWT_ACESS_TOKEN_KEY);
+//     req.user = decode;
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({
+//       success: false,
+//       message: "Invalid token",
+//     });
+//   }
+// };
 
 module.exports.adminRole = (req, res, next) => {
   if (req.user.role !== "admin") {
